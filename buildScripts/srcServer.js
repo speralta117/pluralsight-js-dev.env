3e43311732d72820f134bc4e9891b298e86b73cc
Salvador Peralta <salvadorperalta@MacBookProSpc.local>
2017-11-19 12:43:40 -0600

    fake Data Mock

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -16,7 +16,16 @@ app.use(require('webpack-dev-middleware')(compiler,{
 
 app.get('/', function(req, res){
     res.sendFile(path.join(__dirname, '../src/index.html'));
-})
+});
+
+app.get('/users',function(req, res){
+    res.json([
+        {"id": 1,"firstName":"Bob","lastName":"Smith","email":"bob@gmail.com"},
+        {"id": 2,"firstName":"Tammy","lastName":"Norton","email":"tnorton@yahoo.com"},
+        {"id": 3,"firstName":"Tina","lastName":"Lee","email":"lee.tina@hotmail.com"}
+    ]);
+});
+
 
 app.listen(port, function(err){
     if(err){
--- src/index.js
+++ src/index.js
@@ -1,6 +1,32 @@
-import './index.css';
+import {getUsers, deleteUser} from './api/userApi';
 
-import numeral from 'numeral';
+getUsers().then(result=>{
+    let userBody = "";
 
-const courseValue = numeral(1000).format('$o,0.00');
-console.log(`I would pay ${courseValue} for this awsome course`);//eslint-disable-line no-console
+    result.forEach(user => {
+        userBody+= `<tr>
+                    <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+                    <td>${user.id}</td>
+                    <td>${user.firstName}</td>
+                    <td>${user.lastName}</td>
+                    <td>${user.email}</td>
+                    </tr>`
+    });
+
+    global.document.getElementById('users').innerHTML = userBody;
+
+    const deleteLink = global.document.getElementsByClassName('deleteUser');
+
+    // Must use array.from to create a real array from a DOM collection
+    // getElementsByClassName only returns an "array like" object
+
+    Array.from(deleteLink, link => {
+        link.onclick = function(event) {
+            const element = event.target;
+            event.preventDefault();
+            deleteUser(element.attributes["data-id"].value);
+            const row = element.parentNode.parentNode;
+            row.parentNode.removeChild(row);
+        };
+    });
+});
